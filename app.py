7d3151e0a4076c67cff40a5d500ddfd08d2e1c0e
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-14 20:01:38 +0530

    Completed the logic part

--- app.py
+++ app.py
@@ -8,3 +8,24 @@ app = Flask(__name__) # setting up appname
 def summary_api():
     url = request.args.get('url', '') #fetching the url
     video_id = url.split('=')[1]
+    summary = get_summary(get_transcript(video_id)) #passing the video id to transcript function and passing the whole transcript to the summary function
+    return summary, 200
+
+def get_transcript(video_id): #this transcript return the list of all the transcript provided by YouTube
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+    transcript = ' '.join([d['text'] for d in transcript_list]) #joining all the string to get a whole paragraph
+    return transcript
+
+def get_summary(transcript): #for summarizing I'not implementing the whole model instead of it, using the existing model implementation by Hugging Face
+    summariser = pipeline('summarization') #type of model i.e. here we used 'summarization' model
+    summary = ''
+    for i in range(0, (len(transcript)//1000)+1): #But it has a limitation that it can provide a summary of text that is smaller than 1000 characters, so to remove this limitation we can split the content into multiple parts
+        summary_text = summariser(transcript[i*1000:(i+1)*1000])[0]['summary_text']
+        summary = summary + summary_text + ' '
+    return summary
+
+
+if __name__ == '__main__':
+    app.run()
+
+
